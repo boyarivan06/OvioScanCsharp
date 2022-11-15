5929c32cf42bd21afe10111296fbedd3d9aa6d9b
boyarivan06@gmail.com <boyarivan06@gmail.com>
2022-11-15 18:03:36 +0300

    App can read from file only in debug mode

--- OvioScanCsharp/MainWindow.xaml.cs
+++ OvioScanCsharp/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using IronOcr;
 
 namespace OvioScanCsharp
 {
@@ -27,7 +28,33 @@ namespace OvioScanCsharp
 
         private void runButton_Click(object sender, RoutedEventArgs e)
         {
+            string path = path_box.Text;
+            //string path = "C:/Users/apple/source/repos/OvioScanCsharp/OvioScanCsharp/images/стих.jpg";
+            var Ocr = new IronTesseract();
+            //Ocr.Language = OcrLanguage.Russian;
+            if (file_radio.IsChecked == true && path != String.Empty)
+            {
+                try
+                {
+                    using (var Input = new OcrInput(path))
+                    {
+                        //Input.Deskew();  // use if image not straight
+                        //Input.DeNoise(); // use if image contains digital noise
 
+                        var Result = Ocr.Read(Input);
+                        // Console.WriteLine("End of Ocr.Read");
+                        output_Box.Text = Result.Text; // smth is going on?
+                    }
+                }
+                catch (System.IO.FileNotFoundException)
+                {
+                    warningBox.Text = "Файл не найден";
+                }
+            }
+            else 
+            {
+                warningBox.Text = "Обработка папки пока недоступна";
+            }
         }
     }
 }
